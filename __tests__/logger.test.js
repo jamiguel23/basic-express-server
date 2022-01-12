373ffe40fb19214b34a7633d1b1d7dd1c6cea8aa
jamiguel23 <josematthewmiguel@gmail.com>
2022-01-11 19:06:16 -0800

    testing for 404 on a bad route 404 on a bad method 500 if no name in the query string 200 if the name is in the query string done

--- __tests__/logger.test.js
+++ __tests__/logger.test.js
@@ -4,7 +4,7 @@ const server = require('../lib/server.js');
 const supertest = require('supertest');
 const request = supertest(server.app);
 
-xdescribe('testing logger', () =>{
+describe('testing logger', () =>{
   it('should respond with correct output', async () => {
     const response = await request.get('/person?name=playa');
     expect(typeof response.body).toBe('object');
